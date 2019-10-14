1b7249612ef03fb2ed919dbd52b0e247e8073de1
Michael Henke <433270+aphex3k@users.noreply.github.com>
2019-10-14 15:45:02 -0700

    adding style check for casks

--- Casks/charles-proxy.rb
+++ Casks/charles-proxy.rb
@@ -1,19 +1,19 @@
 cask 'charles-proxy' do
-    version '4.5.1'
-    sha256 'a5052d81f8ed4a5e26ec3a35edfa871053b1c1bc795b3ace30b6d80bf2d77dc7'
+  version '4.5.1'
+  sha256 'a5052d81f8ed4a5e26ec3a35edfa871053b1c1bc795b3ace30b6d80bf2d77dc7'
 
-    url "https://www.charlesproxy.com/assets/release/#{version.gsub(%r{b\d$}, '')}/charles-proxy-#{version}.dmg"
-    name 'Charles'
-    homepage 'https://www.charlesproxy.com/latest-release/'
-  
-    app 'Charles.app'
-  
-    uninstall quit: 'com.xk72.Charles'
-  
-    zap trash: [
-                 '~/Library/Application Support/Charles',
-                 '~/Library/Preferences/com.xk72.Charles.plist',
-                 '~/Library/Preferences/com.xk72.charles.config',
-                 '~/Library/Saved Application State/com.xk72.Charles.savedState',
-               ]
-  end
+  url "https://www.charlesproxy.com/assets/release/#{version.gsub(%r{b\d$}, '')}/charles-proxy-#{version}.dmg"
+  name 'Charles'
+  homepage 'https://www.charlesproxy.com/latest-release/'
+
+  app 'Charles.app'
+
+  uninstall quit: 'com.xk72.Charles'
+
+  zap trash: [
+               '~/Library/Application Support/Charles',
+               '~/Library/Preferences/com.xk72.Charles.plist',
+               '~/Library/Preferences/com.xk72.charles.config',
+               '~/Library/Saved Application State/com.xk72.Charles.savedState',
+             ]
+end
